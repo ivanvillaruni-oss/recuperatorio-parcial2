c8a08565534a428b767ebb61bfdfc6c8a0f295bf
ivanvillaruni-oss <ivan.villar.uni@gmail.com>
2025-11-18 19:19:32 -0300

    arreglo de importacion

--- tests/test_divisionvillar.py
+++ tests/test_divisionvillar.py
@@ -1,5 +1,5 @@
 #tests/test_dividir.py 
-from funciones.dividir import dividir 
+from funciones.divisionvillar import dividir 
 def test_dividir(): 
     assert dividir(10, 2) == 5 
     assert dividir(5, 0) is None
